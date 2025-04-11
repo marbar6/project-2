0b7b9efcb0447927f3a269d8875af680dea4313c
marbar6 <inti.boruah5@gmail.com>
2025-04-11 23:50:05 +0530

    Add all the files using node js

--- server.js
+++ server.js
@@ -5,7 +5,15 @@ const { send } = require("process");
 const app = express();
 const port = 8000;
 
+
+
+// css style 
 app.use(express.static(path.join(__dirname, "Front-branch-name")));
+app.use(express.static(path.join(__dirname,"Front-semester-name/Mechanical/1st-sem")))
+
+
+
+// Front semester
 
 app.get("/",(req,res)=>{
     res.sendFile(path.join("D:/project/PYQ/Front-branch-name","index.html"));
@@ -32,11 +40,34 @@ app.get("/signup.html",(req,res)=>{
 });
 
 
-app.use(express.static(path.join(__filename,"Front-branch-name")))
-
+// mechanical semester 
 app.get("/Front-semester-name/Mechanical/mecha-semester.html",(req,res)=>{
-    res.sendFile(path.join("D:/project/PYQ/Front-semester-name/Mechanical","mecha-semester.html"))
-})
+    res.sendFile(path.join("D:/project/PYQ/Front-semester-name/Mechanical","mecha-semester.html"));
+});
+app.get("/Front-semester-name/Mechanical/1st-sem/1st-sem-year.html",(req,res)=>{
+    res.sendFile(path.join("D:/project/PYQ/Front-semester-name/Mechanical/1st-sem","1st-sem-year.html"));
+});
+app.get("/Front-semester-name/Mechanical/2nd-sem/2nd-sem-year.html",(req,res)=>{
+    res.sendFile(path.join("D:/project/PYQ/Front-semester-name/Mechanical/2nd-sem","2nd-sem-year.html"));
+});
+app.get("/Front-semester-name/Mechanical/3rd-sem/3rd-sem-year.html",(req,res)=>{
+    res.sendFile(path.join("D:/project/PYQ/Front-semester-name/Mechanical/3rd-sem","3rd-sem-year.html"));
+});
+app.get("/Front-semester-name/Mechanical/4th-sem/4th-sem-year.html",(req,res)=>{
+    res.sendFile(path.join("D:/project/PYQ/Front-semester-name/Mechanical/4th-sem","4th-sem-year.html"));
+});
+app.get("/Front-semester-name/Mechanical/5th-sem/5th-sem-year.html",(req,res)=>{
+    res.sendFile(path.join("D:/project/PYQ/Front-semester-name/Mechanical/5th-sem","5th-sem-year.html"));
+});
+app.get("/Front-semester-name/Mechanical/6th-sem/6th-sem-year.html",(req,res)=>{
+    res.sendFile(path.join("D:/project/PYQ/Front-semester-name/Mechanical/6th-sem","6th-sem-year.html"));
+});
+app.get("/Front-semester-name/Mechanical/7th-sem/7th-sem-year.html",(req,res)=>{
+    res.sendFile(path.join("D:/project/PYQ/Front-semester-name/Mechanical/7th-sem","7th-sem-year.html"));
+});
+app.get("/Front-semester-name/Mechanical/8th-sem/8th-sem-year.html",(req,res)=>{
+    res.sendFile(path.join("D:/project/PYQ/Front-semester-name/Mechanical/8th-sem","8th-sem-year.html"));
+});
 
 
 app.listen(port, () => {
