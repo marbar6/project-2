bf6224d8ed9db907381955fb189e71fc009c47de
marbar6 <inti.boruah5@gmail.com>
2025-04-11 22:36:25 +0530

    edit the server in node js

--- server.js
+++ server.js
@@ -1,32 +1,43 @@
 const express = require("express");
 const fs = require("fs");
 const path = require("path");
+const { send } = require("process");
 const app = express();
 const port = 8000;
 
+app.use(express.static(path.join(__dirname, "Front-branch-name")));
 
-app.use(express.static(path.join(__dirname, 'public')));
-
-
-app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, 'index.html'));
+app.get("/",(req,res)=>{
+    res.sendFile(path.join("D:/project/PYQ/Front-branch-name","index.html"));
 });
 
-app.get("/Front-branch-name/index", (req, res) => {
-    res.sendFile(path.join(__dirname, 'index.html'));
+app.get("/index.html",(req,res)=>{
+    res.sendFile(path.join("D:/project/PYQ/Front-branch-name","index.html"));
 });
 
-app.get("/Front-branch-name/index.html", (req, res) => {
-    res.sendFile(path.join(__dirname, 'index.html'));
+app.get("/about.html",(req,res)=>{
+    res.sendFile(path.join("D:/project/PYQ/Front-branch-name/about-page","about.html"));
 });
 
-app.get("/about", (req, res) => {
-    res.sendFile(path.join(__dirname, "about.html"));
+app.get("/profile.html",(req,res)=>{
+    res.sendFile(path.join("D:/project/PYQ/Front-branch-name/profile-page","profile.html"));
 });
 
-app.get("/about.html", (req, res) => {
-    res.sendFile(path.join(__dirname, "about.html"));
+app.get("/signin.html",(req,res)=>{
+    res.sendFile(path.join("D:/project/PYQ/Front-branch-name/profile-page","sign-in.html"));
 });
+
+app.get("/signup.html",(req,res)=>{
+    res.sendFile(path.join("D:/project/PYQ/Front-branch-name/profile-page","sign-up.html"));
+});
+
+
+app.use(express.static(path.join(__filename,"Front-branch-name")))
+
+app.get("/Front-semester-name/Mechanical/mecha-semester.html",(req,res)=>{
+    res.sendFile(path.join("D:/project/PYQ/Front-semester-name/Mechanical","mecha-semester.html"))
+})
+
 
 app.listen(port, () => {
     console.log(`Server running at http://localhost:${port}`);
